077aa168af3dd5bedf694daf82561e9580637224
Aleks Hudochenkov <aleks@hudochenkov.com>
2018-12-22 23:08:27 +0100

    Update false-positive-no-duplicate-selectors/Header.styled.js

--- false-positive-no-duplicate-selectors/Header.styled.js
+++ false-positive-no-duplicate-selectors/Header.styled.js
@@ -2,21 +2,21 @@ import styled from 'styled-components';
 import { ButtonStyled } from '../../button/Button.styled';
 
 export const Left = styled.div`
-  flex: 1;
+  a {
+    color: red;
+  }
 
   ${ButtonStyled} {
-    &:not(:last-child) {
-      margin-right: 8px;
-    }
+    margin-right: 8px;
   }
 `;
 
 export const Right = styled.div`
-  flex: none;
+  a {
+    color: blue;
+  }
 
   ${ButtonStyled} {
-    &:not(:first-child) {
-      margin-left: 8px;
-    }
+    margin-left: 8px;
   }
 `;
